d34c2f00293b1524067240bdd4cb1a15cbd6ff50
Hamolicious <56944714+hamolicious@users.noreply.github.com>
2021-03-11 13:55:50 +0000

    fixed extra bracket

--- extension.js
+++ extension.js
@@ -39,7 +39,7 @@ function getArgs(lineText) {
 	//    def __init__(self, arg1, arg2, arg3):
 	const re = /\(.+\)/;
 	const indentSize = 4; //TODO need to figure out how to read this value from VSCode
-	const indentAmount = (lineText.search(/\S/) + indentSize;
+	const indentAmount = lineText.search(/\S/) + indentSize;
 	lineText = lineText.match(re, '')[0].replace('(self, ', '').replace(')', '').split(', ');
 
 	let text = '';
