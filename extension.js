33008a10ed026427fcfc95e16b5059f533c9e1df
Hamolicious <56944714+hamolicious@users.noreply.github.com>
2021-03-11 17:11:28 +0000

    added tabSize reference instead of hardcoding

--- extension.js
+++ extension.js
@@ -21,14 +21,10 @@ function activate(context) {
 			const classLineText = document.getText(viewRange);
 
 			const edit = getArgs(classLineText);
-
-
-			// Get the word within the selection
 			editor.edit(editBuilder => {
 				editBuilder.replace(editRange, edit);
 			});
 		}
-
 	});
 	context.subscriptions.push(disposable);
 }
@@ -38,8 +34,7 @@ function deactivate() {}
 function getArgs(lineText) {
 	//    def __init__(self, arg1, arg2, arg3):
 	const re = /\(.+\)/;
-	const indentSize = 4; //TODO need to figure out how to read this value from VSCode
-	const indentAmount = lineText.search(/\S/) + indentSize;
+	const indentAmount = lineText.search(/\S/) + vscode.window.activeTextEditor.options.tabSize;
 	lineText = lineText.match(re, '')[0].replace('(self, ', '').replace(')', '').split(', ');
 
 	let text = '';
