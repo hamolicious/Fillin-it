ba0255dba00bc6de728d819022a2dbbae4f171b5
Hamolicious <56944714+hamolicious@users.noreply.github.com>
2021-03-11 13:54:46 +0000

    removed redundant code

--- extension.js
+++ extension.js
@@ -11,8 +11,9 @@ function activate(context) {
 		if (editor) {
 			const document = editor.document;
 
-			const currentLine = editor.selection.active.line;
+			//TODO need to create a better way of finding where the class is
 			const classLine = editor.selection.active.line-1;
+			const currentLine = editor.selection.active.line;
 
 			const viewRange = new vscode.Range(classLine, 0, classLine, Infinity);
 			const editRange = new vscode.Range(currentLine, 0, currentLine, Infinity);
@@ -38,7 +39,7 @@ function getArgs(lineText) {
 	//    def __init__(self, arg1, arg2, arg3):
 	const re = /\(.+\)/;
 	const indentSize = 4; //TODO need to figure out how to read this value from VSCode
-	const indentAmount = ((lineText.search(/\S/) / indentSize) + 1) * indentSize;
+	const indentAmount = (lineText.search(/\S/) + indentSize;
 	lineText = lineText.match(re, '')[0].replace('(self, ', '').replace(')', '').split(', ');
 
 	let text = '';
